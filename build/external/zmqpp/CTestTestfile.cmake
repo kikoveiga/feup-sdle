2c1662efe443b788bfb228a1c960a143bf2cfd86
José Francisco Veiga <veigajosefrancisco@gmail.com>
2024-12-15 18:53:43 +0000

    Delete build folder

--- build/external/zmqpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kikoveiga/Desktop/g14/external/zmqpp
-# Build directory: /home/kikoveiga/Desktop/g14/build/external/zmqpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
